a8910852c51dc1be51d3b01fe29bf66e7232a0a6
Nitin Yadav <ny78455@gmail.com>
2025-09-07 00:51:29 +0530

    backend deployment prereq

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,8 @@
 from app import create_app
+import os
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+    app = create_app()
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
